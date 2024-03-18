862f7a272c44d0c4728a65248ae76556784b1f52
ljh <508328509@qq.com>
2024-03-18 16:10:01 +0800

    relu.s finished

--- src/relu.s
+++ src/relu.s
@@ -14,25 +14,35 @@
 # ==============================================================================
 relu:
     # Prologue
+    addi sp, sp, -8
+    sw s0, 0(sp)
+    sw s1, 4(sp)
+check:
+    addi t0, x0, 1
+    bge a1, t0, loop_prepare # check a1 >= 1
 
-
-loop_start:
-    
-
-
-
-
-
-
-
-loop_continue:
-
-
-
-loop_end:
-
-
+    addi a1, x0, 8 # exit
+    j exit2
+loop_prepare:
+    add s0, a0, x0 # s0 array address
+    add s1, a1, x0 # s1 # of array
+    add t0, x0, x0 # t0 from 0 to #
+loop:
+    beq t0, s1, end
+    slli t1, t0, 2
+    add t2, s0, t1 # t2 is element address
+    lw t3, 0(t2)
+    bge t3, x0, L1
+    add t3, x0, x0
+L1:
+    sw t3, 0(t2)
+    addi t0, t0, 1
+    j loop
+end:
     # Epilogue
+    lw s0, 0(sp)
+    lw s1, 4(sp)
+    addi sp, sp, 8
 
     
 	ret
